740d1c395ba87129958a2d7d839ae94c364b4838
TranTo <totran0@gmail.com>
2020-03-21 15:05:37 +1100

    SQLite added

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import json
+from datetime import datetime
 import os
 import requests
 import urllib
+import sqlite3
 
 from bottle import route, run, redirect, request
 
@@ -9,7 +11,9 @@ REDIRECT_URI=os.environ['REDIRECT_URI']
 CLIENT_ID=os.environ['CLIENT_ID']
 CLIENT_SECRET=os.environ['CLIENT_SECRET']
 SCOPES="r_liteprofile%20r_emailaddress%20w_member_social"
+DB_DIRECTORY=os.environ['DB_DIRECTORY']
 
+conn = None
 
 @route('/')
 def login():
@@ -20,7 +24,7 @@ def login():
              f'scope={SCOPES}')
 
 
-def get_headers(code):
+def get_token(code):
     payload = {
         'grant_type': 'authorization_code',
         'code': code,
@@ -32,15 +36,15 @@ def get_headers(code):
     if r.status_code != 200:
         redirect("/")
     token = r.json()['access_token']
-    return {"Authorization": f"Bearer {token}"}
+    return token
 
 
-def get_name(headers):
+def get_name_and_id(headers):
     r = requests.get("https://api.linkedin.com/v2/me", headers=headers)
     if r.status_code != 200:
         redirect("/")
     rj = r.json()
-    return f'{rj["localizedFirstName"]} {rj["localizedLastName"]}'
+    return f'{rj["localizedFirstName"]} {rj["localizedLastName"]}', rj["id"]
 
 
 def get_profile_pic_url(headers):
@@ -59,16 +63,27 @@ def get_profile_pic_url(headers):
 
 @route('/callback')
 def callback():
+    global conn
     code = request.params.get('code')
     if code is None:
         redirect("/")
 
-    headers = get_headers(code)
-    name = get_name(headers)
+    token = get_token(code)
+    headers = {"Authorization": f"Bearer {token}"}
+    name, userid = get_name_and_id(headers)
     profile_pic_url = get_profile_pic_url(headers)
-
+    c = conn.cursor()
+    insert = (userid, str(datetime.now()), name, token)
+    c.execute("INSERT INTO users VALUES(?,?,?,?) ON CONFLICT(id) DO UPDATE SET date=excluded.date, name=excluded.name, token=excluded.token",insert)
+    conn.commit()
     return f'Your token: <code>{headers["Authorization"]}</code><p>Hello {name}</p><p><img src="{profile_pic_url}"></img></p>'
 
 
 if __name__ == "__main__":
+    db_filename = os.path.join(DB_DIRECTORY, 'auth.db')
+    conn = sqlite3.connect(db_filename)
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS users 
+                 (id text UNIQUE, date text, name text, token text)''')
+    conn.commit()
     run(host='0.0.0.0', port=8082, debug=True)
